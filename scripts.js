61941a8f2a9ffe908c9dd06a506c5dd191215650
amir <amfar77@gmail.com>
2023-06-14 00:31:36 -0400

    Add full operation and screen update functionality

--- scripts.js
+++ scripts.js
@@ -8,6 +8,8 @@ function divide (a,b) {return a / b}
 let firstN = 0;
 let operator = '';
 let secondN = 0;
+let operatorSelcted = false;
+let operatorCounter = 0;
 
 // Create operate function that takes in above variables and returns correct output
 function operate(firstN,operator,secondN) {
@@ -24,11 +26,55 @@ function operate(firstN,operator,secondN) {
 }
 
 
+// Numbers show on screen when pressed
+const screen = document.querySelector('.screen')
+const screenValue = document.querySelector('.numberScreen')
+const zeroScreen = document.querySelector('.zeroScreen')
+screenValue.innerHTML = ''
+zeroScreen.innerHTML = '0'
 
+for (let i=0; i<10; i++) {
+    document.querySelector(`.a${i}`).addEventListener('click',() => {
+        if(operatorSelcted) {
+            screenValue.innerHTML = '';
+            zeroScreen.innerHTML = '0';
+            operatorSelcted = false
+        }
+        screenValue.innerHTML += `${i}`;
+        zeroScreen.innerText = '';
+    });
+}
 
+// Add clear button
+document.querySelector(".clearAll").addEventListener('click', clearScreen)
 
+function clearScreen() {
+    screenValue.innerHTML = '';
+    zeroScreen.innerHTML = '0';
+    operatorCounter = 0;
+}
 
+// Operator buttons
+document.querySelector(".operators").addEventListener('click', operatorFunc)
 
+function operatorFunc(event) {
+    if(operatorCounter>0) {
+        equals();
+    }
 
+    operatorSelcted = true;
+    const button = event.target;
+    operator = button.value;
+    firstN = Number(screenValue.innerHTML);
+    operatorCounter++;
 
-console.log(operate(4,'/',2))
+    
+}
+
+// Equal button
+document.getElementById("equals").addEventListener('click', equals)
+
+function equals() {
+    secondN = Number(screenValue.innerHTML);
+    screenValue.innerHTML = Math.round( ( (operate(firstN,operator,secondN)) * 100000 )) / 100000 ;
+}
